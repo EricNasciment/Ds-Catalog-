b8584ccbfd16bae4c1b5c1d4af4f22b0a74d470f
Eric Silva <ericsilva075@gmail.com>
2026-09-16 15:59:16 -0300

    Refactor endpoint findAllPage in Product and Category

--- Backend/ds/src/main/java/com/Dscatalog/ds/Resources/ProductResources.java
+++ Backend/ds/src/main/java/com/Dscatalog/ds/Resources/ProductResources.java
@@ -6,6 +6,7 @@ import com.Dscatalog.ds.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,15 +23,8 @@ public class ProductResources {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDto>> findAllPage(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ){
-        PageRequest pageRequest =  PageRequest.of(page,linesPerPage,
-                Sort.Direction.valueOf(direction),orderBy);
-        Page<ProductDto> list = productService.findPagedAll(pageRequest);
+    public ResponseEntity<Page<ProductDto>> findAllPage(Pageable pageable){
+        Page<ProductDto> list = productService.findPagedAll(pageable);
         return ResponseEntity.ok().body(list);
     }
 
